77e0488cab3fd8707e82c5a81103c214945fd5e2
Elrey Belmonti and Megan Olson <megan@>
2017-09-25 10:32:46 -0700

    display list on index page

--- app.rb
+++ app.rb
@@ -7,3 +7,17 @@ require("./lib/department")
 require("./lib/project")
 require("pg")
 require("pry")
+
+
+get('/') do
+  @departments = Department.all
+  erb(:index)
+end
+
+post('/') do
+  dep_name = params["dep_name"]
+  department = Department.new({:name => dep_name})
+  department.save
+  @departments = Department.all
+  erb(:index)
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,17 @@ require 'pg'
 require 'employee'
 require 'department'
 require 'project'
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Department.all().each() do |department|
+      department.destroy()
+    end
+    Employee.all.each do |employee|
+      employee.destroy
+    end
+    Project.all.each do |project|
+      project.destroy
+    end
+  end
+end
